e9e4944ffadb226c8bde78c95d029027bc24d9e2
Sean <seanysean700@gmail.com>
2018-01-29 19:27:24 -0400

    Simple clocks

--- js/board.js
+++ js/board.js
@@ -1,4 +1,8 @@
 var chess = new Chess(),
+    clocks = {
+        white: 14,
+        black: 14
+    },
     config = {
     coordinates: false,
     movable: {
@@ -12,20 +16,25 @@ var chess = new Chess(),
     highlight: {
         check: true,
         lastMove: true
+    },
+    animation: {
+        enabled: false
     }
 },  sound = {
     capture: new Audio('https://lichess1.org/assets/sound/standard/Capture.ogg'),
     gameOver: new Audio('https://lichess1.org/assets/sound/standard/Victory.ogg'),
     move: new Audio('https://lichess1.org/assets/sound/standard/Move.ogg')
-},  cground = Chessground(document.getElementById('cg'),config);
+},  ended = false, cground = Chessground(document.getElementById('cg'),config);
 function aiMove(chess,cg) {
     return (orig,dest) => {
         var obj = { from: orig, to: dest,promotion: 'q' };
         var m = chess.move(obj);
+        if (chess.game_over()) ended = true;
         check(chess,cground);
+        pressClock();
         makeSound(m.flags);
         if (m.flags.includes('p')) promote(cground,dest,'queen');
-        if (!chess.game_over() && chess.turn() === 'b') {
+        if (chess.turn() === 'b' && !ended) {
             setTimeout(() => {
                 var moves = chess.moves({ verbose: true });
                 var move = moves[Math.floor(Math.random() * moves.length)];
@@ -39,14 +48,14 @@ function aiMove(chess,cg) {
                     }
                 });
                 if (m2.flags.includes('p')) promote(cground,m2.to,rToRook(m2.promotion));
+                if (chess.game_over()) gameEnd();
                 check(chess,cground);
+                pressClock();
                 makeSound(m2.flags);
                 cg.playPremove();
             }, 300);
         } else {
-            cg.set({
-                viewOnly: true
-            });
+            gameEnd();
         }
     }
 }
@@ -99,14 +108,42 @@ function check(c,cg) {
         };
     }
 }
-function makeSound(m) {
-    if (chess.game_over()) {
+function makeSound(m = '') {
+    if (ended) {
+        sound.gameOver.volume = 0.1;
         sound.gameOver.play();
     } else if (m.includes('c')) {
+        sound.capture.volume = 0.1;
         sound.capture.play();
     } else {
+        sound.move.volume = 0.1;
         sound.move.play();
     }
+}
+var interv = setInterval(()=>console.log('nothing'),10000),
+    toMove = 'white';
+function pressClock() {
+    toMove = toMove === 'white' ? 'black' : 'white';
+    var html = {
+        black: document.getElementById('clock1'),
+        white: document.getElementById('clock2')
+    }
+    window.clearInterval(interv);
+    if (!ended) {
+        interv = setInterval(() => {
+            clocks[toMove] = (clocks[toMove] - 0.1).toFixed(1);
+            html[toMove].innerHTML = '0:' + (clocks[toMove] > 9 ? '' : '0') + clocks[toMove];
+            if (clocks[toMove] === (0).toFixed(1)) gameEnd();
+        }, 100);
+    }
+}
+function gameEnd() {
+    window.clearInterval(interv);
+    ended = true;
+    makeSound();
+    cground.set({
+        viewOnly: true
+    });
 }
 cground.set({
     movable: {
